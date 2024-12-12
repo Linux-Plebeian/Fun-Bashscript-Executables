d65367b5a4e12f7b7fd3fa6809d4c41b2e8be670
Coding-Plebeian <linuxnoob373@gmail.com>
2024-12-12 00:37:40 +0000

    Yapperbot.sh

--- Yapperbot.sh
+++ Yapperbot.sh
@@ -1,10 +1,10 @@
 while :
 do
-    read a
-    yn=(
-        "yes"
-        "no"
-    )
+    yn=( "Did you know the fear of long words is called Hippopotomonstrosesquippedaliophobia", 
+    "The hill with the longest name in the world is Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu, having 85 letters"
+    "Did you know that Silicon, the element that makes up the transistors in your chipsets on your pc, laptop, tablet, phone, etc. is found in nature as SiO₂, which is what makes up things like quartz?"
+    "Fun Fact, cows have 4 stomachs, the rumen, the reticulum, the omasum and the abomasum!"
+    "Did you know that Nes games were coded in pure 6502 assembly, with only 53 possible instructions and a 256-byte stack?")
     ryn=${yn[RANDOM % ${#yn[@]}]}
     echo '------------------'
     echo  $ryn
@@ -14,5 +14,5 @@ do
     echo '                     |  /  / |'
     echo '                     |   \__/|'
     echo '                      -------'
-    read b
+    sleep 1
 done
